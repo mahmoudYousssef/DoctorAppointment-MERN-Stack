cda1f72b38e2437de6373e6feb39595524ae668b
Ma7moud Youssef <ma7moudyoussef21@gmail.com>
2025-09-14 10:50:29 +0300

    create user route

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -1,18 +1,23 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  name:String ,
+  name: {
+    type: String,
+    require: true,
+  },
   email: {
-    type:String,
-    uniqe:true 
+    type: String,
+    uniqe: true,
+    require: true,
   },
-  password:{
-    type:String
+  password: {
+    type: String,
+    require: true,
   },
-  role:{
-    type:String,
-    default:'user'
-  }
-  
-})
-const User = mongoose.model('User', UserSchema);
+  role: {
+    type: String,
+    default: "user",
+  },
+});
+const User = mongoose.model("User", UserSchema);
+export default User;
